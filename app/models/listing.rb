8472501f5a3071dab6bee0a2be916d1e435bf65d
evorick <rickkadlac@gmail.com>
2015-09-10 21:21:33 -0500

    added simple validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,6 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
   has_many :reservations, dependent: :destroy
+
+  validates :user, :name, :address, :description, :availability_from, :availability_to, :sqft, :price, presence: true
 end
