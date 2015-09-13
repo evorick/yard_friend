bfb86641cfd53f1fdaad981b5c66b5d06f8a492a
evorick <rickkadlac@gmail.com>
2015-09-13 17:06:31 -0500

    stuck with the rent controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -10,6 +10,7 @@ class ListingsController < ApplicationController
   # GET /listings/1
   # GET /listings/1.json
   def show
+    @listings = listing.find(params[:id])
   end
 
   # GET /listings/new
--- app/controllers/rent_controller.rb
+++ app/controllers/rent_controller.rb
@@ -1,4 +1,9 @@
 class RentController < ApplicationController
   def yards
+    @rents = Listing.all
+  end
+
+  def show
+    @rents = listing.find(params[:id])
   end
 end
