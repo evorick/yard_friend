b8e81523121fa150cea4c78b187400bd2ae8bfaa
evorick <rickkadlac@gmail.com>
2015-09-14 10:21:50 -0500

    setup paperclip gem in models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,9 @@ class Listing < ActiveRecord::Base
   has_many :reservations, dependent: :destroy
 
   validates :user, :name, :address, :description, :availability_from, :availability_to, :sqft, :price, presence: true
+  validates :name, uniqueness: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+  has_attached_file :image, styles: { large: "600x600>", medium: "300x300#", thumb: "100x100#" }, default_url: ":style_missing.jpg"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
