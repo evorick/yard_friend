71a151ca48f8f84ac9d43c868a2575f559b91799
evorick <rickkadlac@gmail.com>
2015-09-14 18:01:14 -0500

    set date validation to not allow a date before current date

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,8 @@ class Listing < ActiveRecord::Base
   validates :user, :name, :address, :description, :availability_from, :availability_to, :sqft, :price, presence: true
   validates :name, uniqueness: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :availability_from, 
+  validates :availability_from, :timeliness => {:on_or_after => lambda { Date.current }, :type => :date}
+  validates :availability_to, :timeliness => {:on_or_after => lambda { Date.current }, :type => :date}
 
   has_attached_file :image, styles: { large: "600x600>", medium: "300x300#", thumb: "100x100#" }, default_url: ":style_missing.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
