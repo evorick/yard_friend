66797bf5d09080f30b12106a42750bf5c8b323c5
evorick <rickkadlac@gmail.com>
2015-09-14 19:23:12 -0500

    worked on default image

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,6 +8,12 @@ class Listing < ActiveRecord::Base
   validates :availability_from, :timeliness => {:on_or_after => lambda { Date.current }, :type => :date}
   validates :availability_to, :timeliness => {:on_or_after => lambda { Date.current }, :type => :date}
 
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300#", thumb: "100x100#" }, default_url: ":style_missing.jpg"
+  has_attached_file :image, styles: { 
+    large: "600x600>", 
+    medium: "300x300#", 
+    thumb: "100x100#" }, 
+    #TODO FIX THE DEFAULT IMAGE URL BELOW.
+    default_url: "/images/default_:style.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
+
